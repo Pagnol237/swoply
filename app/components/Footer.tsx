b3aef3a448f6fb2b646ce82a6748ebf22622ae13
Hey there! I'm franck pagnol Nounjo <99295070+Pagnol237@users.noreply.github.com>
2025-10-29 18:18:47 -0500

    add
    fotter add

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className={Style.footerMain}>
         <p>© 2025 Swoply — Tous droits réservés<br/>
-            <span style={{fontWeight:300,fontSize:15}}>Développé par <Link href="https://franckpagnol.vercel.app/" target='_blank'><u>Nounjo franck</u></Link> </span>
+            <span style={{fontWeight:300,fontSize:15}}>Développé et designer par <Link href="https://franckpagnol.vercel.app/" target='_blank'><u>Nounjo franck</u></Link> </span>
         </p>
     </div>
   )
